565693b00710616c400efb49faedf0cd7bcdcd66
c0dexter <michal.dobrowolski1@gmail.com>
2019-06-19 00:03:13 +0200

    Initial commit with a new project

---
+++ app/src/main/java/pl/c0dexter/dagge2tutorial/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.c0dexter.dagge2tutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
